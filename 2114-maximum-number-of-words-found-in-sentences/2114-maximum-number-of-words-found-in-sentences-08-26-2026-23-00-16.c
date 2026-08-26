3f68ae34b9b87b7afdf88f079d842093fa02429c
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-08-26 23:00:16 +0530

    Time: 4 ms (4.58%), Space: 9.6 MB (33.42%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences-08-26-2026-23-00-16.c
@@ -0,0 +1,18 @@
+int mostWordsFound(char** sentences, int sentencesSize)
+{
+    int max = 0;
+    for (int i = 0; i < sentencesSize; i++)
+    {
+        int count = 1;
+        for (int j = 0; sentences[i][j] != '\0'; j++)
+        {
+            if (sentences[i][j] == ' ')
+                count++;
+        }
+        if(count > max)
+        {
+            max = count;
+        }
+    }
+    return max;
+}
