025e8b92215cd4af0ed47b715fa6d274e38f4fb0
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-08-28 00:44:15 +0530

    Time: 0 ms (100%), Space: 8.7 MB (31.45%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero-08-28-2026-00-44-16.c
@@ -0,0 +1,17 @@
+int numberOfSteps(int num) {
+    int count = 0;
+    while(num!=0)
+    {
+        if(num%2==0)
+        {
+            count++;
+            num /= 2;
+        }
+        else
+        {
+            count++;
+            num -= 1;
+        }
+    }
+    return count;
+}
