d885d47392f5ed8e4331f3699430374f7212e81c
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-14 17:33:55 +0530

    Time: 2 ms (32.3%), Space: 58.6 MB (69.91%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation-09-14-2026-17-33-54.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+    const n = nums.length;
+    const ans = new Array(n);
+    for (let i = 0; i < n; i++) {
+        ans[i] = nums[nums[i]];
+    }
+    return ans;
+};
