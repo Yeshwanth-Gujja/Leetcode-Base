a59bf9df9e6804c76fc8232e23c078893c946fd9
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-15 00:12:16 +0530

    Time: 0 ms (100%), Space: 54.6 MB (6.63%) - LeetHub

---
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number-09-15-2026-00-12-15.js
@@ -0,0 +1,16 @@
+var countDigits = function(num) {
+    let org = num;
+    let digit = 0;
+    let count = 0;
+    while (num > 0)
+    {
+        digit = num % 10;
+        num = Math.floor(num / 10);
+
+        if (org % digit === 0)
+        {
+            count++;
+        }
+    }
+    return count;
+};
