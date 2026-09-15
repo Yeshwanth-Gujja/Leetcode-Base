d6d1e5b6662c2227216a38ed6725b37889f77343
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-15 11:50:07 +0530

    Time: 2 ms (43.14%), Space: 59.4 MB (44.12%) - LeetHub

---
+++ 2942-find-words-containing-character/2942-find-words-containing-character-09-15-2026-11-50-06.ts
@@ -0,0 +1,9 @@
+function findWordsContaining(words: string[], x: string): number[] {
+    let res: number[] = [];
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].includes(x)) {
+            res.push(i);
+        }
+    }
+    return res;
+}
