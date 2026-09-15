da5dd53eb1dd60e7cd16b4ef02225d67f183fd72
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-15 11:12:38 +0530

    Time: 0 ms (100%), Space: 54 MB (76.92%) - LeetHub

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string-09-15-2026-11-12-38.ts
@@ -0,0 +1,9 @@
+function countSegments(s: string): number {
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] != ' ' && (i == 0 || s[i - 1] == ' ')) {
+            count++;
+        }
+    }
+    return count;
+};
