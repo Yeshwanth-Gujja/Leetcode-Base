4d653bdcfbadac25fe1a21fde8bba77beef1df43
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-15 14:59:03 +0530

    Time: 37 ms (82.6%), Space: 52.1 MB (97.34%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address-09-15-2026-14-59-02.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function(address) {
+    let defang = "";
+    for(let i=0; i<address.length; i++){
+        if(address[i]=="."){
+            defang += "[.]"; 
+        }
+        else{
+            defang += address[i];
+        }
+    }
+    return defang;
+};
