32355434e630e6603f8a8f5428f2d1f2ed6b6b33
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-15 12:39:52 +0530

    Time: 0 ms (100%), Space: 8.1 MB (83.08%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case-09-15-2026-12-39-51.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for (int i = 0; s[i] != '\0'; i++) {
+            if(s[i]>64 && s[i]<91){
+                s[i] = s[i] + 32;
+            } 
+        }
+        return s;
+    }
+};
