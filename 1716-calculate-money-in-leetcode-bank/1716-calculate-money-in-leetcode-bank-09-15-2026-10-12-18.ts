255484e731d8a24bb4e82ce1e07a15e339e40bd5
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-15 10:12:19 +0530

    Time: 0 ms (100%), Space: 55.5 MB (43.75%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank-09-15-2026-10-12-18.ts
@@ -0,0 +1,12 @@
+function totalMoney(n: number): number {
+    let weeks = Math.floor(n / 7);
+    let days = n % 7;
+    let amount = 0;
+    for (let i = 0; i < weeks; i++) {
+        amount += 28 + (7 * i);
+    }
+    for (let i = weeks + 1; i <= weeks + days; i++) {
+        amount += i;
+    }
+    return amount;
+};
