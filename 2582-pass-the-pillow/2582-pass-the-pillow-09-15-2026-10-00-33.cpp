4e0af755eea6b9f80bf65dc0d366890d4fa906cc
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-15 10:00:34 +0530

    Time: 0 ms (100%), Space: 7.9 MB (9.18%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow-09-15-2026-10-00-33.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int direction = time/(n-1);
+        int position = time%(n-1);
+        return direction%2==0 ? position+1 : n-position;
+    }
+};
