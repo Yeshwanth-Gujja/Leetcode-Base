c2755cd5d53a286b117d377cbdf9c1c8ae6a4ca0
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-15 14:52:42 +0530

    Time: 0 ms (100%), Space: 8.5 MB (14%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address-09-15-2026-14-52-41.c
@@ -0,0 +1,18 @@
+char * defangIPaddr(char * address){
+    int len = strlen(address);
+    int new_len = len + 2 * 3; 
+    char *result = (char*)malloc((new_len + 1) * sizeof(char));
+    int j = 0;
+    
+    for (int i = 0; i < len; i++) {
+        if (address[i] == '.') {
+            result[j++] = '[';
+            result[j++] = '.';
+            result[j++] = ']';
+        } else {
+            result[j++] = address[i];
+        }
+    }
+    result[j] = '\0';
+    return result;
+}
