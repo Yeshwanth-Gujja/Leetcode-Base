a515eb2c863fb857ec30b5b7ad229ae623271ab5
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-16 21:29:24 +0530

    Time: 0 ms (100%), Space: 56.8 MB (48.84%) - LeetHub

---
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string-09-16-2026-21-29-24.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var removeTrailingZeros = function(num) {
+    let i = num.length - 1;
+    while (num[i] == '0')
+        i--;
+    return num.slice(0, i + 1);
+};
