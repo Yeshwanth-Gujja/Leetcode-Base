7ad45cbc34988e27d628ac01979bf0c38fa23013
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-16 23:59:06 +0530

    Time: 0 ms (100%), Space: 55.5 MB (67.57%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones-09-16-2026-23-59-06.ts
@@ -0,0 +1,15 @@
+function numJewelsInStones(jewels: string, stones: string): number {
+    let count = 0;
+    let character_array = new Array(128).fill(0);
+    let i = 0;
+    while (i < stones.length){
+        character_array[stones.charCodeAt(i)] += 1;
+        i += 1;
+    }
+    i = 0;
+    while (i < jewels.length){
+        count += character_array[jewels.charCodeAt(i)];
+        i += 1;
+    }
+    return count;
+};
