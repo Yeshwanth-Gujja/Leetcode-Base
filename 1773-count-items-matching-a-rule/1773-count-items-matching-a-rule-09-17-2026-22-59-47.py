d307fc6b861a0b391bde49cac5d7da8f05e137be
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-17 22:59:46 +0530

    Time: 0 ms (100%), Space: 23.8 MB (10.91%) - LeetHub

---
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule-09-17-2026-22-59-47.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        index = {"type": 0, "color": 1, "name": 2}[ruleKey]
+        count = 0
+
+        for item in items:
+            if item[index] == ruleValue:
+                count += 1
+
+        return count
