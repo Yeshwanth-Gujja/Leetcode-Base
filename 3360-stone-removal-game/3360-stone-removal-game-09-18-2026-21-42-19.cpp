a47da62f9e556eaf913be4d5494f2799249b52cb
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-18 21:42:19 +0530

    Time: 0 ms (100%), Space: 7.7 MB (49.26%) - LeetHub

---
+++ 3360-stone-removal-game/3360-stone-removal-game-09-18-2026-21-42-19.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canAliceWin(int n) {
+        int repitation = 10;
+        bool alice_winner = false;
+        while(n>=repitation){
+            n -= repitation;
+            alice_winner = !(alice_winner);
+            repitation--;
+        }
+        return alice_winner;
+    }
+};
