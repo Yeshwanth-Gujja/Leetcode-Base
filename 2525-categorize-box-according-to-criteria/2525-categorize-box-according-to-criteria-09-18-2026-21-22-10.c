edd695046a16153d6350bfcaa5489a7f64174533
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-18 21:22:11 +0530

    Time: 0 ms (100%), Space: 8.4 MB (60.9%) - LeetHub

---
+++ 2525-categorize-box-according-to-criteria/2525-categorize-box-according-to-criteria-09-18-2026-21-22-10.c
@@ -0,0 +1,15 @@
+char* categorizeBox(int length, int width, int height, int mass) {
+    bool bulky = (length >= 10000 ||
+                  width  >= 10000 ||
+                  height >= 10000 ||
+                  (long long)length * width * height >= 1000000000LL);
+
+    bool heavy = (mass >= 100);
+    if (bulky && heavy)
+        return "Both";
+    if (bulky)
+        return "Bulky";
+    if (heavy)
+        return "Heavy";
+    return "Neither";
+}
