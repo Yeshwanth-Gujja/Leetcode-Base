e7ace9b3bd704f3a28b3631f358494774df351fa
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-19 07:17:14 +0530

    Time: 0 ms (100%), Space: 58.7 MB (11.66%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz-09-19-2026-07-17-12.js
@@ -0,0 +1,23 @@
+var fizzBuzz = function(n) {
+    let result = new Array(n);
+    let i = 1;
+
+    while (i <= n) {
+        if (!(i % 3) && !(i % 5)) {
+            result[i - 1] = "FizzBuzz";
+        }
+        else if (!(i % 3)) {
+            result[i - 1] = "Fizz";
+        }
+        else if (!(i % 5)) {
+            result[i - 1] = "Buzz";
+        }
+        else {
+            result[i - 1] = String(i);
+        }
+
+        i++;
+    }
+
+    return result;
+};
