07172c249154bce87283be628c01b6c618261b99
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-20 22:27:22 +0530

    Time: 0 ms (100%), Space: 53.1 MB (96.85%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square-09-20-2026-22-27-21.ts
@@ -0,0 +1,15 @@
+function isPerfectSquare(num: number): boolean {
+    let left = 1;
+    let right = num;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        let square = mid * mid;
+        if (square === num)
+            return true;
+        else if (square < num)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return false;
+}
