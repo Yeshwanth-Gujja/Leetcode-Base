e0b8c28121014a326f2eeedb88687aa812d1d0bb
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-20 16:26:52 +0530

    Time: 0 ms (100%), Space: 55.3 MB (96.55%) - LeetHub

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference-09-20-2026-16-26-51.ts
@@ -0,0 +1,16 @@
+function differenceOfSums(n: number, m: number): number {
+    let num1 = 0;
+    let num2 = 0;
+    for(let i=0;i<n+1;i++)
+    {
+        if (i%m!=0)
+        {
+            num1 = num1+i;
+        }
+        else
+        {
+            num2 = num2+i;
+        }
+    }
+    return (num1-num2);
+};
