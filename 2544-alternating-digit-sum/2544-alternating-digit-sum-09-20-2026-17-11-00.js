7a00ad19f1691b4241c0dc4338c84ea731818bc8
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-20 17:11:01 +0530

    Time: 0 ms (100%), Space: 53.7 MB (40.68%) - LeetHub

---
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum-09-20-2026-17-11-00.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var alternateDigitSum = function(n) {
+    let sum=0;
+    let sign=1;
+    while(n>0){
+        sum=sum+sign*(n%10);
+        sign=-sign;
+        n = Math.floor(n/10);
+    }
+    return -sign*sum;
+};
