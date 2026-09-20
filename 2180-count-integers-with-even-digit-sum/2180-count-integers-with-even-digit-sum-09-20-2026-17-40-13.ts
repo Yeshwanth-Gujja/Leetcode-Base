1db5aeafa40babc317dee278c1120c2af0f3c35b
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-20 17:40:14 +0530

    Time: 0 ms (100%), Space: 55.2 MB (60%) - LeetHub

---
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum-09-20-2026-17-40-13.ts
@@ -0,0 +1,12 @@
+function countEven(num: number): number {
+    let sum = 0, temp = num;
+    while (temp > 0)
+    {
+        sum += temp % 10;
+        temp = Math.floor(temp/10);
+    }
+    if (sum % 2 == 0)
+        return Math.floor(num / 2);
+    else
+        return Math.floor((num - 1) / 2);
+};
