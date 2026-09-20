225a8b0d96fc91a94b1a787cc093b8c830380561
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-20 13:22:34 +0530

    Time: 0 ms (100%), Space: 55.7 MB (100%) - LeetHub

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i-09-20-2026-13-22-34.ts
@@ -0,0 +1,15 @@
+function sumAndMultiply(n: number): number {
+    let x = 0;
+    let sum = 0;
+    let mul = 1;
+    while (n > 0) {
+        let m = n % 10;
+        n = Math.floor(n / 10);
+        if (m === 0)
+            continue;
+        sum = sum + m;
+        x = m * mul + x;
+        mul = mul * 10;
+    }
+    return x * sum;
+};
