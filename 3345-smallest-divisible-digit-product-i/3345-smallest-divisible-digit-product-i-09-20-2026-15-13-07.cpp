6e6f2042c502533a4f9e3a4f51ddfc1e466f3a03
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-20 15:13:08 +0530

    Time: 0 ms (100%), Space: 8.6 MB (41.84%) - LeetHub

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i-09-20-2026-15-13-07.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+        while (true)
+        {
+            int pro = 1;
+            int m = 0;
+            int org = n;
+            while (n>0)
+            {
+                pro = pro*(n%10);
+                n = n/10;
+            }
+            if (pro%t==0)
+            {
+                return org;
+            }
+            n = org+1;
+        }
+    }
+};
