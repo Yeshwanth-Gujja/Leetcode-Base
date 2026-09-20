1c9850dc28a0e240020b7ec25a421f8f9b517a60
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-20 11:57:09 +0530

    Time: 2 ms (20.96%), Space: 57.7 MB (6.11%) - LeetHub

---
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer-09-20-2026-11-57-09.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var mirrorDistance = function(n) {
+    let org = n;
+    let rev = 0;
+    while(n>0){
+        rev = rev*10 + n%10;
+        n = Math.floor(n/10);
+    }
+    return Math.abs(org - rev);
+};
