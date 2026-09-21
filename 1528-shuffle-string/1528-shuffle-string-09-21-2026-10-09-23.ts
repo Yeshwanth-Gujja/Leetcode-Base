5f7da95b9c8dd51589bf94ebe8e21453ff4bc3b4
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-21 10:09:24 +0530

    Time: 3 ms (15.25%), Space: 57.5 MB (69.49%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string-09-21-2026-10-09-23.ts
@@ -0,0 +1,7 @@
+function restoreString(s: string, indices: number[]): string {
+    let res = new Array(s.length);
+    for (let i = 0; i < s.length; i++) {
+        res[indices[i]] = s[i];
+    }
+    return res.join("");
+};
