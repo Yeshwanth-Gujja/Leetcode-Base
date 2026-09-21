e2cecd71ce302223deffdd9ea5be9e9cbf741c11
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-21 10:29:34 +0530

    Time: 1 ms (30.43%), Space: 55.8 MB (21.74%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds-09-21-2026-10-29-33.ts
@@ -0,0 +1,12 @@
+function threeConsecutiveOdds(arr: number[]): boolean {
+    let count = 0;
+    for(let i = 0; i<arr.length; i++){
+        if(arr[i]%2)
+            count+=1;
+        else
+            count = 0;
+        if(count == 3)
+            return true;
+    }
+    return false;
+};
