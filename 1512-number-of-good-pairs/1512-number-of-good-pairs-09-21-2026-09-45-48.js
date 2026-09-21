9a5119314f25d125828133affe8ace5db8fa61a6
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-21 09:45:49 +0530

    Time: 1 ms (51.13%), Space: 54 MB (31.22%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs-09-21-2026-09-45-48.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    let count = 0;
+    let hash = new Array(101).fill(0);
+    for (let i = 0; i < nums.length; i++) {
+        hash[nums[i]]++;
+    }
+    for (let i = 0; i < 101; i++){
+        count += ((hash[i]) * (hash[i] - 1))/2;
+    }
+    return count;
+};
