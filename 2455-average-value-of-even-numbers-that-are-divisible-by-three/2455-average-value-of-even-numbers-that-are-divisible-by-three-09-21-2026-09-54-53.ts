96fc9eda8101214fb45c23c7815450859d7df607
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-21 09:54:54 +0530

    Time: 0 ms (100%), Space: 55.8 MB (80%) - LeetHub

---
+++ 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three-09-21-2026-09-54-53.ts
@@ -0,0 +1,15 @@
+function averageValue(nums: number[]): number {
+    let sums = 0;
+    let count = 0;
+    for(let i = 0; i<nums.length; i++)
+    {
+        if(nums[i]%6 == 0)
+        {
+            sums += nums[i];
+            count++;
+        }
+    }
+    if(count == 0)
+        return 0;
+    return Math.floor(sums/count);
+};
