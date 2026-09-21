a6a4725058aa768059040206d8d56c3c148d56d9
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-21 10:17:08 +0530

    Time: 1 ms (85.19%), Space: 57.6 MB (94.81%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity-09-21-2026-10-17-07.ts
@@ -0,0 +1,23 @@
+function sortArrayByParity(nums: number[]): number[] {
+    let ans = [];
+    let i = 0;
+
+    while (i < nums.length) {
+        if (nums[i] % 2 === 0) {
+            ans.push(nums[i]);
+            nums[i] = 0;
+        }
+        i++;
+    }
+
+    i = 0;
+
+    while (i < nums.length) {
+        if (nums[i] !== 0) {
+            ans.push(nums[i]);
+        }
+        i++;
+    }
+
+    return ans;
+};
