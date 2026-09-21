fbb02856c5a00ef835a999e8736a3b93308d44d5
Yeshwanth Gujja <gujjayeshwanthofficial@gmail.com>
2026-09-21 09:36:56 +0530

    Time: 2 ms (68.79%), Space: 60 MB (87.54%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones-09-21-2026-09-36-55.ts
@@ -0,0 +1,19 @@
+function findMaxConsecutiveOnes(nums: number[]): number {
+    let maximum = 0, count = 0;
+    for(let i = 0; i<nums.length; i++)
+    {
+        if(nums[i]==1)
+        {
+            count+=1;
+            if(count>maximum)
+            {
+                maximum = count;
+            }
+        }
+        else
+        {
+            count = 0;
+        }
+    }
+    return maximum;
+};
